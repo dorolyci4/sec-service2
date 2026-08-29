855c18e88c29eb4f3696bd8a109e79ea35c8827f
Alexis <dorolyci4@gmail.com>
2026-08-29 17:43:07 -0400

    Jwt de base

--- src/main/java/org/socom/secservice/config/SecurityConfig.java
+++ src/main/java/org/socom/secservice/config/SecurityConfig.java
@@ -1,13 +1,16 @@
 package org.socom.secservice.config;
 
+import org.socom.secservice.filters.JwtAuthenticationFilter;
 import org.socom.secservice.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,24 +20,49 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
+    //private final JwtAuthenticationFilter jwtAuthenticationFilter; // 🔥 Injecter le filtre
 
     // Injection du UserDetailsService créé à l'étape 1
     public SecurityConfig(CustomUserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
+        //this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .formLogin(Customizer.withDefaults())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated() // Autorise l'accès à toutes les pages (y compris H2 et vos APIs)
+                // Désactiver la protection CSRF car elle repose sur les sessions
+                .csrf(AbstractHttpConfigurer::disable) // Requis pour pouvoir utiliser la console H2 ou faire des POST/PUT, csrf utilise la session
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // Définir la politique de session sur STATELESS
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                //.csrf(AbstractHttpConfigurer::disable) // Requis pour pouvoir utiliser la console H2 ou faire des POST/PUT
-                .headers(headers -> headers.frameOptions(frame -> frame.disable())); // Requis pour afficher l'interface H2 dans les frames
+                // 🔥 DÉSACTIVER formLogin (cause principale des 302)
+                // .formLogin(Customizer.withDefaults())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll() //// Autorise l'accès à toutes les pages (y compris H2 et vos APIs)
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                // Ajouter le filtre JWT
+                .addFilter(jwtAuthenticationFilter())// 🔥 Ajouter le filtre d'authentification (gère le login)
+                // Requis pour afficher l'interface H2 dans les frames
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
         return http.build();
     }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        return new JwtAuthenticationFilter(authenticationManager(null));
+    }
+
+
+    // 🔥 Bean pour créer le filtre avec l'AuthenticationManager
 
     // Déclaration de l'encodeur de mot de passe utilisé pour la vérification
     @Bean
@@ -44,7 +72,7 @@ public class SecurityConfig {
 
     // Bean obligatoire si vous gérez le login manuellement via un Controller REST
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) {
         return authConfig.getAuthenticationManager();
     }
 
---
+++ src/main/java/org/socom/secservice/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,83 @@
+package org.socom.secservice.filters;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+//@Component
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    //private AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.setAuthenticationManager(authenticationManager);
+        this.setFilterProcessesUrl("/api/auth/login");
+    }
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        System.out.println("attemptAuthentication");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username == null || password == null) {
+            System.out.println("❌ Username ou password null !");
+            throw new RuntimeException("Username et password requis");
+        }
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, password);
+
+
+        try {
+            Authentication auth = this.getAuthenticationManager().authenticate(authenticationToken);
+            System.out.println("✅ Authentification réussie !");
+            return auth;
+        } catch (Exception e) {
+            System.out.println("❌ Erreur d'authentification: " + e.getMessage());
+            throw e;
+        }
+
+
+        // return authenticationManager.authenticate(authenticationToken);
+    }
+
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+        System.out.println("successfulAuthentication");
+        User user = (User) authResult.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("mysecret1234");
+        assert user != null;
+        String jwtAccessToken = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 5 * 60 * 1000))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .sign(algorithm);
+
+
+        response.addHeader("Authorization", jwtAccessToken);  // ✅ CORRECT
+        //response.addHeader("Authorization", "Bearer " + jwtAccessToken);
+        //response.setContentType("application/json");
+        //response.getWriter().write("{\"token\": \"" + jwtAccessToken + "\"}");
+        // 🔥 BODY - Ajoutez ces 3 lignes pour voir le token dans Postman
+        response.setContentType("application/json");
+        response.getWriter().write("{\"token\": \"" + jwtAccessToken + "\"}");
+    }
+}
